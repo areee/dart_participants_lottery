85e4ec751cd8f0c0baa9abeb832309b626cdd28b
Arttu Ylhävuori <10089872+areee@users.noreply.github.com>
2021-06-18 21:41:29 +0300

    First version based on my Lua code (#1)
    * add a base for reordered list & random number lottery
    
    * Finish a raw version of the code (based on Lua)

--- bin/dart_participants_lottery.dart
+++ bin/dart_participants_lottery.dart
@@ -1,7 +1,31 @@
-void main(List<String> arguments) {
-  var names = 'Mike, Helen, Terry, Catherine, Max';
+import 'dart:math';
 
+void main(List<String> arguments) {
+  var result = _runLottery('Mike, Helen, Terry, Catherine, Max');
+  print(result);
+}
+
+// Lottery function
+String _runLottery(String names) {
   var namesList = names.split(', ');
 
-  print(namesList);
+  var reOrderedList = <int>[];
+
+  var random = Random();
+
+  while (reOrderedList.length != namesList.length) {
+    var randomNumber = random.nextInt(namesList.length);
+
+    if (!reOrderedList.contains(randomNumber)) {
+      reOrderedList.add(randomNumber);
+    }
+  }
+
+  var returnedString = '';
+
+  for (var i in reOrderedList) {
+    returnedString = '$returnedString ${namesList[i]}';
+  }
+
+  return returnedString;
 }
