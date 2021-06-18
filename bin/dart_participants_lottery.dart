b37fbcb6257380c4aafcde1dd36a5c6a5b1374be
areee <10089872+areee@users.noreply.github.com>
2021-06-18 20:18:01 +0300

    add a base for reordered list & random number lottery

--- bin/dart_participants_lottery.dart
+++ bin/dart_participants_lottery.dart
@@ -1,7 +1,21 @@
+import 'dart:math';
+
 void main(List<String> arguments) {
+  // names list
   var names = 'Mike, Helen, Terry, Catherine, Max';
-
   var namesList = names.split(', ');
-
   print(namesList);
+
+  // reordered list
+  var orderedList = <String>[];
+
+  orderedList.add('test 1');
+  orderedList.add('test 2');
+  print(orderedList);
+
+  // random numbers
+  var random = Random();
+  for (var i = 0; i < namesList.length; i++) {
+    print(random.nextInt(namesList.length));
+  }
 }
