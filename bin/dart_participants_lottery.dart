f77fbe8f56fdd556b3e9e276b92d373a00bea3dc
areee <10089872+areee@users.noreply.github.com>
2021-06-20 21:01:49 +0300

    Add test for upcoming "write input feature"

--- bin/dart_participants_lottery.dart
+++ bin/dart_participants_lottery.dart
@@ -1,6 +1,11 @@
+import 'dart:io';
 import 'package:dart_participants_lottery/lottery_logic.dart' as lottery;
 
-void main(List<String> arguments) {
-  var result = lottery.runLottery('Mike, Helen, Terry, Catherine, Max');
-  print(result);
+void main(/*List<String> arguments*/) {
+  stdout.writeln('Type something');
+  final input = stdin.readLineSync();
+  stdout.writeln('You typed: $input');
+
+  // var result = lottery.runLottery('Mike, Helen, Terry, Catherine, Max');
+  // print(result);
 }
