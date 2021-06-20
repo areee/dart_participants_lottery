d5df2b9ae9c5f3b172ed530d1e9a55e42a54ddb5
areee <10089872+areee@users.noreply.github.com>
2021-06-20 20:50:06 +0300

    Move lottery logic into own file

--- bin/dart_participants_lottery.dart
+++ bin/dart_participants_lottery.dart
@@ -1,35 +1,6 @@
-import 'dart:math';
+import 'package:dart_participants_lottery/lottery_logic.dart' as lottery;
 
 void main(List<String> arguments) {
-  var result = _runLottery('Mike, Helen, Terry, Catherine, Max');
+  var result = lottery.runLottery('Mike, Helen, Terry, Catherine, Max');
   print(result);
-}
-
-// Lottery function
-String _runLottery(String names) {
-  var namesList = names.split(',');
-
-  var reOrderedList = <int>[];
-
-  var random = Random();
-
-  while (reOrderedList.length != namesList.length) {
-    var randomNumber = random.nextInt(namesList.length);
-
-    if (!reOrderedList.contains(randomNumber)) {
-      reOrderedList.add(randomNumber);
-    }
-  }
-
-  var returnedString = '';
-
-  for (var i = 0; i < reOrderedList.length; i++) {
-    if (i == 0) {
-      returnedString = '${namesList[i]}';
-    } else {
-      returnedString = '$returnedString, ${namesList[i]}';
-    }
-  }
-
-  return returnedString.replaceAll('  ', ' ');
 }
---
+++ lib/lottery_logic.dart
@@ -0,0 +1,29 @@
+import 'dart:math';
+
+String runLottery(String names) {
+  var namesList = names.split(',');
+
+  var reOrderedList = <int>[];
+
+  var random = Random();
+
+  while (reOrderedList.length != namesList.length) {
+    var randomNumber = random.nextInt(namesList.length);
+
+    if (!reOrderedList.contains(randomNumber)) {
+      reOrderedList.add(randomNumber);
+    }
+  }
+
+  var returnedString = '';
+
+  for (var i = 0; i < reOrderedList.length; i++) {
+    if (i == 0) {
+      returnedString = '${namesList[i]}';
+    } else {
+      returnedString = '$returnedString, ${namesList[i]}';
+    }
+  }
+
+  return returnedString.replaceAll('  ', ' ');
+}
